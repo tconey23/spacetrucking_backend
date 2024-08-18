444bde1f0c33f10311e19d123c4cd966e6708f68
Tom Coney <tom.ce.coney@gmail.com>
2024-08-17 19:59:04 -0600

    Add POST /api/cargo endpoint

--- index.js
+++ index.js
@@ -34,3 +34,7 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+setTimeout(() => {
+    console.log('app running')
+}, 1000);
